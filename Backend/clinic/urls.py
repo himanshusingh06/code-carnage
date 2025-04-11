fbacf2d6272be8c66b870ecdc75e706d9a6ec3bf
himanshusingh06 <himanshuks062@gmail.com>
2025-04-11 22:36:21 +0530

    updated apis

--- Backend/clinic/urls.py
+++ Backend/clinic/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("create", views.ClinicAPIView.as_view(), name="clinic"),
-    path('get-doctors', views.DoctorListAPIView.as_view(), name='doctor-list'),
+    path("create/", views.ClinicAPIView.as_view(), name="clinic-listcreate"),
+    path('get-doctors/', views.DoctorListAPIView.as_view(), name='doctor-list'),
     path('doctors/create', views.DoctorCreateAPIView.as_view(), name='doctor-create'),
     path('doctors/delete/<int:id>/', views.DoctorDeleteAPIView.as_view(), name='doctor-delete'),
 ]
